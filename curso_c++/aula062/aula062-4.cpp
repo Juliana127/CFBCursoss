af5a0f668208fec5fedd05350f1a4450d031e53c
Juliana <jd391829@gmail.com>
2024-05-30 22:44:30 -0300

    05/30/2024
    Ponteiro inteligente

--- curso_c++/aula062/aula062-4.cpp
+++ curso_c++/aula062/aula062-4.cpp
@@ -18,8 +18,9 @@ int main() {
 
 	shared_ptr <Carro> c1(new Carro);
 	shared_ptr <Carro> c2 = c1;
+	//shared_ptr<Carro> c2(new Carro);
 	c1->setVel(10);
-	c2->setVel(5);
+	c2->setVel(5); //ambos recebem o mesmo valor, e sendo esse o ultimo será valor 5, pois ambos estão usando o mesmo ponteiro a não ser que faça dois ponteiros
 	cout << "Velocidade: " << c1->getVel() << endl;
 	cout << "Velocidade: " << c2->getVel() << endl;
 
